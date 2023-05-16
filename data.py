464ab1a297fef8e642062851bb0699736b273038
patrickohara06 <patrickohara06@outlook.com>
2023-05-16 14:22:08 -0800

    Go

--- data.py
+++ data.py
@@ -1,9 +1,55 @@
-WEBSITE_LIST = {"https://www.youtube.com/":
+WEBSITE_LIST = {"https://www.facebook.com/":
                 {"specifics":"ablock=true;",
-                 "main_menu":'refresh_sens:id;guide-icon',
                  "endpoints":
                  { 
-                  "login-page":   'direct-link;signin',
+                  "places":   'partial link text;Places',
+                  "games":    'partial link text;Games',
+                  "marketplace": 'partial link text;Marketplace',
+                  "meta pay":   'partial link text;Meta Pay',
+                  "meta store": 'partial link text;Meta Store',
+                  "meta quest": 'partial link text;Meta Quest',
+                  "instagram":  'partial link text;Instagram',
+                  "bulletin":   'partial link text;Bulletin',
+                  "fundraisers":    'partial link text;Fundraisers',
+                  "services":   'partial link text;Services',
+                  "voting information center":  'partial link text;Voting Information Center',
+                  "privacy policy": 'partial link text;Privacy Policy',
+                  "privacy center": 'partial link text;Privacy Center',
+                  "groups": 'partial link text;Groups',
+                  "about":  'partial link text;About',
+                  "create ad":  'partial link text;Create Ad',
+                  "developers": 'partial link text;Developers',
+                  "careers":    'partial link text;Careers',
+                  "cookies":    'partial link text;Cookies',
+
+
+
+
+
+                 },
+                  "sub-endpoints":
+                  {
+                      "main_menu":{"trending":'relies_prev:partial link text;Trending',
+                                   "music":   'relies_prev:partial link text;Music',
+                                   "gaming":  'relies_prev:partial link text;Gaming',
+                                   "news":    'relies_prev:partial link text;News',
+                                   "sports":  'relies_prev:partial link text;Sports',
+                                   "learning":'relies_prev:partial link text;Learning'
+                                   },
+                      "places":{"video":'rand_ind:css selector;ytd-video-renderer'
+                                  },
+                      "music":{"video":'rand_ind:css selector;ytd-video-renderer'
+                               },
+                      "gaming":{"video":"rand_ind:css selector;ytd-video-renderer"
+                          
+                      },
+                  }
+                },
+                "next website":
+                {"specifics":"ablock=true;",
+                 "endpoints":
+                 { 
+                  "places":   'partial link text;Places',
                   "video":  'css selector;ytd-rich-item-renderer'
                  },
                   "sub-endpoints":
@@ -15,7 +61,7 @@ WEBSITE_LIST = {"https://www.youtube.com/":
                                    "sports":  'relies_prev:partial link text;Sports',
                                    "learning":'relies_prev:partial link text;Learning'
                                    },
-                      "trending":{"video":'rand_ind:css selector;ytd-video-renderer'
+                      "places":{"video":'rand_ind:css selector;ytd-video-renderer'
                                   },
                       "music":{"video":'rand_ind:css selector;ytd-video-renderer'
                                },
@@ -23,5 +69,5 @@ WEBSITE_LIST = {"https://www.youtube.com/":
                           
                       },
                   }
-                 }
+                }
                 }

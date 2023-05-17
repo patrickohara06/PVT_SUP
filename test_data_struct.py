0c474a45671ee143c077f7b1ba9d532bfd9f4729
patrickohara06 <patrickohara06@outlook.com>
2023-05-17 11:24:55 -0800

    jheez

--- test_data_struct.py
+++ test_data_struct.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
     #c_driver = create_edge_driver(ublock=True, headless=False)
     c_driver = create_chrome_driver(ublock=True, headless=False)
     website_to_test = "https://www.facebook.com/"
-    seleniumsselector_list = ["partial link text;Cookies"] # "refresh_sens:id;guide-icon", "relies_prev:partial link text;Trending",
+    seleniumsselector_list = ["partial link text;Meta Store","partial link text;Support"] # "refresh_sens:id;guide-icon", "relies_prev:partial link text;Trending",
     #seleniumsselector_list = ["direct-link;signin", "partial link text;Create account"]
     test_ss_list(website_to_test, seleniumsselector_list, c_driver)
     #test_struct("https://www.youtube.com/")

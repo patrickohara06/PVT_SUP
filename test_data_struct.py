96d88e3d7bb280565abaf4c1b6c76b6aeeec023b
patrickohara06 <patrickohara06@outlook.com>
2023-05-25 11:17:58 -0800

    leggoo

--- test_data_struct.py
+++ test_data_struct.py
@@ -228,8 +228,8 @@ def test_ss_list(website_url:str, ss_list:list, driver):
 if __name__ == "__main__":
     #c_driver = create_edge_driver(ublock=True, headless=False)
     c_driver = create_chrome_driver(ublock=True, headless=False, notifications=True)
-    website_to_test = "https://www.reddit.com/"
-    seleniumsselector_list = ["partial link text;Gaming","partial link text;User Agreement",] # "refresh_sens:id;guide-icon", "relies_prev:partial link text;Trending",
+    website_to_test = "https://www.amazon.com/"
+    seleniumsselector_list = ["partial link text;Careers","partial link text;Job Categories",] # "refresh_sens:id;guide-icon", "relies_prev:partial link text;Trending",
     #seleniumsselector_list = ["direct-link;signin", "partial link text;Create account"]
     test_ss_list(website_to_test, seleniumsselector_list, c_driver)
     #test_struct("https://www.youtube.com/")
